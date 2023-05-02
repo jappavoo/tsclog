1dbfa3c205ee9e8bb4f2987c02a902dcb2ebf869
Jonathan Appavoo <jappavoo@bu.edu>
2023-05-02 03:31:32 +0000

    started in memory logging support

--- tsclog.c
+++ tsclog.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <err.h>
 #include <sched.h>
 #include <sys/sysinfo.h>
@@ -8,6 +9,27 @@
 #include "now.h"
 #include "cacheline.h"
 
+#define LOG_CPU
+#define LOG_TID
+
+struct logentry {
+#ifdef LOG_CPU
+  uint32_t cpu;
+#endif
+#ifdef LOG_TID
+  uint32_t tid;
+#endif
+  uint64_t tsc;
+  uint64_t data[];
+};
+
+struct Log {
+  struct Info {
+    uint64_t len;
+    uint64_t *cur;
+  } info;
+  struct logentry entries[];
+};
 
 void pinCpu(int cpu)
 {
@@ -121,6 +143,45 @@ Java_tsclog_stderr_1label_1now(JNIEnv *env,
   (*env)->ReleaseStringUTFChars(env, label, cp);
   return t;
 }
+
+JNIEXPORT jlong JNICALL
+Java_tsclog_mklog(JNIEnv *env, jclass jcl, jlong n)
+{
+  uint64_t mem = 0;
+  if (n) {
+    mem = (uint64_t)malloc(n*sizeof(uint64_t));
+  }
+  return mem;
+}
+
+JNIEXPORT void JNICALL
+Java_tsclog_log(JNIEnv *env, jclass jcl, jlong lptr)
+{
+}
+
+JNIEXPORT void JNICALL
+Java_tsclog_log1(JNIEnv *, jclass, jlong lptr, jlong v1)
+{
+}
+
+
+#if 0
+// Get a reference to this object's class
+   jclass thisClass = (*env)->GetObjectClass(env, thisObj);
+ 
+   // int
+   // Get the Field ID of the instance variables "number"
+   jfieldID fidNumber = (*env)->GetFieldID(env, thisClass, "number", "I");
+   if (NULL == fidNumber) return;
+ 
+   // Get the int given the Field ID
+   jint number = (*env)->GetIntField(env, thisObj, fidNumber);
+   printf("In C, the int is %d\n", number);
+ 
+   // Change the variable
+   number = 99;
+   (*env)->SetIntField(env, thisObj, fidNumber, number);
+#endif
 
 #else
 
--- tsclog.java
+++ tsclog.java
@@ -9,12 +9,30 @@ public class tsclog
     public static native long stdout_label_now(String label);
     public static native long stderr_now();
     public static native long stderr_label_now(String label);
+    public static native long mklog(long n);
+    public static native void log(long lptr);
+    public static native void log1(long lptr, long v1);
     
-
+    private long logptr = 0;
+    
     static {
 	System.loadLibrary("tsclog");
     }
 
+    public tsclog(long n) {
+	logptr = mklog(n);
+	System.out.println("logptr: 0x" + Long.toUnsignedString(logptr,16));
+    }
+
+    public void log() {
+	tsclog.log(logptr);
+
+    }
+
+    public void log1(long v1) {
+	tsclog.log1(logptr,v1);
+    }
+    
     public static void main(String[] args)
     {
 	int avail = tsclog.availcpus();
@@ -36,6 +54,9 @@ public class tsclog
 	tsclog.stdout_now();
 	tsclog.stderr_now();	
 	tsclog.stdout_label_now("mapper1");
-	tsclog.stderr_label_now("mapper2");	
+	tsclog.stderr_label_now("mapper2");
+	
+	tsclog log = new tsclog(1000);
+	
     }
 }
